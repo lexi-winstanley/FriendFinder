11ff7c406451a4b7659c122836d8a4841b539d49
lexi-winstanley <50344054+lexi-winstanley@users.noreply.github.com>
2019-08-24 16:11:00 -0700

    Was calculating the max difference when needed to be the minimum

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,31 +7,35 @@ module.exports = function (app) {
         res.json(friendsData);
     });
 
-    app.post("/api/friends", function(req, res) {
+    app.post("/api/friends", function (req, res) {
         friendsData.push(req.body);
         console.log(req.body);
         res.json(friendsData[findMatch()]);
-      });
+    });
 }
 
 function findMatch() {
-            let currentUserScores = friendsData[friendsData.length-1].scores;
-            let arrayDifferences = [];
-            for (let i=0; i < friendsData.length-1; i++) {
-                let sumDifferences = 0;
-                let currentComparison = friendsData[i].scores;
-                for (let j=0; j < 10; j++) {
-                    sumDifferences += Math.abs(currentUserScores[j] - currentComparison[j]);
-                }
-                arrayDifferences.push({friendsIndex : i, differenceToUser : sumDifferences});
-            }
-            let maxDifference = 0;
-            let maxDifferenceIndex = 0;
-            for(let k = 0; k < arrayDifferences.length; k++) {
-              if(arrayDifferences[k].differenceToUser > maxDifference) {
-                maxDifference = arrayDifferences[k].differenceToUser;
-                maxDifferenceIndex = arrayDifferences[k].friendsIndex;
-              }
-            }
-            return maxDifferenceIndex;
+    let currentUserScores = friendsData[friendsData.length - 1].scores;
+    let arrayDifferences = [];
+    for (let i = 0; i < friendsData.length - 1; i++) {
+        let sumDifferences = 0;
+        let currentComparison = friendsData[i].scores;
+        for (let j = 0; j < 10; j++) {
+            sumDifferences += Math.abs(currentUserScores[j] - currentComparison[j]);
+            console.log(sumDifferences);
         }
+        arrayDifferences.push({ friendsIndex: i, differenceToUser: sumDifferences });
+    }
+    let minDifference = 51;
+    let minDifferenceIndex = 0;
+    for (let k = 0; k < arrayDifferences.length; k++) {
+        if (arrayDifferences[k].differenceToUser < minDifference) {
+            minDifference = arrayDifferences[k].differenceToUser;
+            minDifferenceIndex = arrayDifferences[k].friendsIndex;
+            console.log(arrayDifferences[k].differenceToUser);
+            console.log(arrayDifferences[k].friendsIndex);
+            console.log(minDifference);
+        }
+    }
+    return minDifferenceIndex;
+}
