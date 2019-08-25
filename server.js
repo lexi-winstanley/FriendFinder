0ebabedd5f2559c8f99e9be9071c94210324d7d7
lexi-winstanley <50344054+lexi-winstanley@users.noreply.github.com>
2019-08-24 17:01:11 -0700

    Added favicon, adjusted blue of buttons, increased width of dropdowns

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ let PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('./app/public'));
+
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
